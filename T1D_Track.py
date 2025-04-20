55c5b902d0fc0e5fd69ebfe24745e898085baca7
Sierra Dean <35184706+SierraD@users.noreply.github.com>
2025-04-20 04:33:42 -0700

    Update T1D_Track.py

--- T1D_Track.py
+++ T1D_Track.py
@@ -3,9 +3,6 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-[connections.gsheets]
-spreadsheet = "https://docs.google.com/spreadsheets/d/111nDe7ePRJaJ1TpSBkEK87sae92dNLLZUNaWCPyLZrI/edit?gid=0#gid=0"
-
 conn = st.connection("T1D_Track", type=GSheetsConnection)
 df = conn.read()
 st.write(df)
