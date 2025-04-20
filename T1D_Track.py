6049bf8edf8ecb12e67469242903f98baacb0e87
Sierra Dean <35184706+SierraD@users.noreply.github.com>
2025-04-20 04:48:09 -0700

    Update T1D_Track.py

--- T1D_Track.py
+++ T1D_Track.py
@@ -3,6 +3,9 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+st.header("T1D Tracking")
+st.write("V0; 2025-04-20")
+
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
 st.write(df)
