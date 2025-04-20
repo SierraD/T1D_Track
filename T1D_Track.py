5a3dc377e2685c6235e19a3cc4e2341e8aea5c34
Sierra Dean <35184706+SierraD@users.noreply.github.com>
2025-04-20 05:19:23 -0700

    Update T1D_Track.py

--- T1D_Track.py
+++ T1D_Track.py
@@ -10,12 +10,13 @@ st.write("V0; 2025-04-20")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read()
-# st.write(data)
 
 df_WO = pd.DataFrame(data, columns=("%dHR" % i for i in range(25)))
 st.write(df_WO)
 df_W = pd.DataFrame(data, columns=("%dHRW" % i for i in range(25)))
 st.write(df_W)
+
+st.write(df_W.name)
 
 # st.scatter_chart(
 #     chart_data,
