a60d926d302edf750bb6f657f4ee91af55d5ec5b
Sierra Dean <35184706+SierraD@users.noreply.github.com>
2025-04-20 05:53:43 -0700

    Update T1D_Track.py

--- T1D_Track.py
+++ T1D_Track.py
@@ -15,8 +15,8 @@ data = conn.read()
 df_WO = pd.DataFrame(data, columns=("%dHR" % i for i in range(25)))
 df_WO = df_WO.T
 # st.write(df_WO)
-df_W = pd.DataFrame(data, columns=("%dHRW" % i for i in range(25)))
-df_W = df_W.T
+# df_W = pd.DataFrame(data, columns=("%dHRW" % i for i in range(25)))
+# df_W = df_W.T
 # st.write(df_W)
 
 chart = st.scatter_chart(df_WO)
